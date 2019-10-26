8ae5b40dda66d67c9f19ae886fad57e02be87b5b
Paige McAuliffe <paigemca@lawn-128-61-35-223.lawn.gatech.edu>
2019-10-26 15:26:34 -0400

    adding detail page

---
+++ src/Details.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Details() {
+  return (
+    <div className="Details">
+      <header className="Scan more items">
+        <p> HackGT6!! </p>
+      </header>
+    </div>
+);
+}
+
+export default Details;
