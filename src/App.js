211c6514134eb1f510e4bc36fbf62032f1a65263
Lauren Jeanne Wetherhead <lwetherhead1@student.gsu.edu>
2019-10-26 10:40:00 -0400

    checking account

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p> Hack GT YAY </p>
-        <p> small change to check credentials :)  </p>
+        <p> change to check credentials :)  </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
